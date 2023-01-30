90f3ca5d9f7330fb919efac01e6cf585d6e7aa3b
motuncoded <motunrayoadeneye@example.com>
2023-01-30 23:28:13 +0100

    React essential training

--- reactjs/react-essentials/src/App.js
+++ reactjs/react-essentials/src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App(props) {
+function App({name}) {
   return (
     <div className="App">
-      <h1>Hello {props.library}</h1>
+      <h1>Hello {name}</h1>
     </div>
   );
 }
